521b1e722845eccdfe245419a7bf2168d44bab9c
Oleg Braginsky <gobrol@gmail.com>
2014-10-14 16:42:40 +0300

    list is returned from sort + error added to json

--- sort_app/views.py
+++ sort_app/views.py
@@ -13,16 +13,16 @@ def sort(request):
         try:
             res = [float(x) for x in res]
         except:
-            return HttpResponse(json.dumps({'result': 'Wrong input'}), content_type="application/json")
+            return HttpResponse(json.dumps({'error': 'Wrong input.'}), content_type="application/json")
     else:
         return render(request, 'sort.html')
-    res = ', '.join(str(x) for x in _bubble_(res))
-    return HttpResponse(json.dumps({'result': res}), content_type="application/json")
+    # res = ', '.join(str(x) for x in _bubble_(res))
+    return HttpResponse(json.dumps({'result': _bubble_(res)}), content_type="application/json")
 
 
-def _bubble_(list):
-    for i in range(len(list)):
-        for j in range(i+1, len(list)):
-            if list[j] < list[i]:
-                list[j], list[i] = list[i], list[j]
-    return list
+def _bubble_(l):
+    for i in range(len(l)):
+        for j in range(i+1, len(l)):
+            if l[j] < l[i]:
+                l[j], l[i] = l[i], l[j]
+    return l
